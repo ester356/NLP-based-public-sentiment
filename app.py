e196eb9806f64776eb0cda0a2e27367cba0f8fb1
es <2351482820@qq.com>
2025-07-01 17:03:09 +0800

    1

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 import flask
+
+
+
+
+app = Flask(__name__)
+app.secret_key = 'this is secret_key you know ?'
+
+
+
+if __name__ == '__main__':
+    # app.run()
+    socketio.run(app, allow_unsafe_werkzeug=True,debug=True)
